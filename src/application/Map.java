a73be0ef32508f824097b09aabee0701f7fad7f7
Kristopher Poirier <poirierk2@wit.edu>
2022-03-04 13:18:51 -0500

    Map Basic Functions Outline

--- src/application/Map.java
+++ src/application/Map.java
@@ -1,5 +1,45 @@
 package application;
 
-public class Map {
+import java.util.ArrayList;
 
+public class Map {
+	Tile[][] tiles;
+	ArrayList<Entity> entities = new ArrayList<Entity>();
+	
+	public Map(int r, int c) {
+		tiles = new Tile[r][c];
+	}
+	
+	public Map(Tile[][] tiles) {
+		this.tiles = tiles;
+	}
+	
+	//implement loading a map from a file
+	//public Map() {
+	//	
+	//}
+	
+	public Tile getTile(int r, int c) {
+		return tiles[r][c];
+	}
+	
+	public void setTile(int r, int c, Tile tile) {
+		tiles[r][c] = tile;
+	}
+	
+	public void addEntity(ArrayList<Entity> entities) {
+		
+	}
+	
+	public void addEntity(Entity entity) {
+		
+	}
+	
+	public ArrayList<Entity> getEntities() {
+		return entities;
+	}
+	
+	public boolean removeEntity(int x, int y) {
+		return false;
+	}
 }

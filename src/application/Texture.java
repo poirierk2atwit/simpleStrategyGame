ad149dd3fbc22bcc445746ef6f0dd8ff76d13996
smithc35 <smithc35@PF2Z2L1Q.wit.private>
2022-04-09 12:49:24 -0400

    Textures

--- src/application/Texture.java
+++ src/application/Texture.java
@@ -1,13 +1,28 @@
 package application;
 
-public class Texture {
-	//public static <type>[] textures; //All textures will be loaded here as a public static to avoid loading them multiple times in memory. Format tbd.
-	
-	/**
-	 * To be run at the start of the main. Will load all textures into the textures variable.
-	 */
-	public static void loadTextures() {
-		
-	}
-	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import java.util.HashMap;
+import java.util.Map;
+import javafx.scene.image.Image;
+
+public class Texture extends Application {
+	    public Map<String, Image> HashMap =  new HashMap<String, Image>();
+	    Stage stage = new Stage();
+	    
+	    public Texture() {
+	    	start(stage);
+	    }
+	    
+	    public void start(Stage primaryStage){
+	    	HashMap.put("name1", new Image(name));
+	        HashMap.put("name2", new Image(name));
+	        HashMap.put("name3", new Image(name));
+	        HashMap.put("name4", new Image(name));
+	    }
+	    
+	    public Image getTexture(String key) {
+	    	return HashMap.get(key);
+	    }
 }
+
